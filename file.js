340ec948d2e5d15b0e36c32be95dbfc593ba3079
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-11 08:00:58 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,16 +1,23 @@
 (function () {
- var url = "https://www.tutorialspoint.com/"; 
- $.ajax({
-        type: 'GET',
-        dataType: 'json',
-        url: url,
-
-        success: function(data){
-          return data;
-        },
-
-        error: function(data){
-          console.log(data.responseText);
-        }
-    });
+    // create dialog elements
+    const dialog = document.createElement("ui5-dialog");
+    const content = document.createElement("div");
+    const footer = document.createElement("footer");
+    const footerBtn = document.createElement("ui5-button");
+    // set attributes
+    dialog.setAttribute("header-text", "Third Party JavaScript");
+    footer.setAttribute("slot", "footer");
+    // content
+    content.innerHTML = "This dialog was created by third party JS file";
+    content.style.padding = "1rem";
+    // footer btn
+    footerBtn.onclick = () => dialog.close();
+    footerBtn.innerHTML = "Close";
+    // append elements
+    dialog.appendChild(content);
+    dialog.appendChild(footer);
+    footer.appendChild(footerBtn);
+    document.body.appendChild(dialog);
+    // open dialog
+    dialog.show();
 })();
