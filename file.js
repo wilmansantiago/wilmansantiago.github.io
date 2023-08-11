e9f897264e476ed584d86dfc1160d5a72f364ef2
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-11 08:39:12 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,13 +1,35 @@
 (function () {
-    setServerStatus("unknown");
-    var img = document.body.appendChild(document.createElement("img"));
-    img.onload = function()
-    {
-        setServerStatus("online");
-    };
-    img.onerror = function()
-    {
-        setServerStatus("offline");
-    };
-    img.src = "http://myserver.com/ping.gif";
+    // create dialog elements
+    const dialog = document.createElement("ui5-dialog");
+    const content = document.createElement("div");
+    const footer = document.createElement("footer");
+    const footerBtn = document.createElement("ui5-button");
+
+     var pingserver = new Image();
+   pingserver.src = "https://lbcldwqase04.bancolombia.corp/sap/bc/webdynpro/sap/zwd_homepage_mashup_sf#";
+
+   if (pingserver.height>0) {
+      alert("Online");
+        window.location.href = "https://performancemanager8.successfactors.com/xi/ui/payroll/pages/PayStatement.xhtml?bplte_company=bancolombiT4&_s.crb=Plq1mCfJTZQJCHgjc8Sq%252b3iDu2qPpaTvMGEBIh4SJwU%253d&mashup=true&encodedJSONEvent=UHJ1ZWJhIGRlIGNvbmV4aW9u&userId=MDAwMTc1Mjk%3D&legalEntityId=00000001&eou=true";
+   } else {
+      alert("Offline");
+        window.location.href = "https://performancemanager8.successfactors.com/xi/ui/payroll/pages/PayStatement.xhtml?bplte_company=bancolombiT4&_s.crb=Plq1mCfJTZQJCHgjc8Sq%252b3iDu2qPpaTvMGEBIh4SJwU%253d&mashup=true&encodedJSONEvent=UHJ1ZWJhIGRlIGNvbmV4aW9u&userId=MDAwMTc1Mjk%3D&legalEntityId=00000001&eou=true";   
+       // set attributes
+    dialog.setAttribute("header-text", "You should are connected to VPN");
+    footer.setAttribute("slot", "footer");
+    // content
+    content.innerHTML = "Dear User, You should are connected to VPN";
+    content.style.padding = "1rem";
+    // footer btn
+    footerBtn.onclick = () => dialog.close();
+    footerBtn.innerHTML = "Close";
+    // append elements
+    dialog.appendChild(content);
+    dialog.appendChild(footer);
+    footer.appendChild(footerBtn);
+    document.body.appendChild(dialog);
+    // open dialog
+    dialog.show(); 
+   }
+   
 })();
