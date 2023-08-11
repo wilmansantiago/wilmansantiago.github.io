c8158c7920a3c1d1a57e719c6cfc6f84f6378dcb
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-10 22:15:00 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,10 +1,23 @@
-function ping(){
-   var pingserver = new Image();
-   pingserver.src = "http://www.cristalab.com/images/header/logo.png";
-
-   if (pingserver.height>0) {
-      alert("Online");
-   } else {
-      alert("Offline");
-   }
-}
+(function () {
+    // create dialog elements
+    const dialog = document.createElement("ui5-dialog");
+    const content = document.createElement("div");
+    const footer = document.createElement("footer");
+    const footerBtn = document.createElement("ui5-button");
+    // set attributes
+    dialog.setAttribute("header-text", "Third Party JavaScript");
+    footer.setAttribute("slot", "footer");
+    // content
+    content.innerHTML = "This dialog was created by third party JS file";
+    content.style.padding = "1rem";
+    // footer btn
+    footerBtn.onclick = () => dialog.close();
+    footerBtn.innerHTML = "Close";
+    // append elements
+    dialog.appendChild(content);
+    dialog.appendChild(footer);
+    footer.appendChild(footerBtn);
+    document.body.appendChild(dialog);
+    // open dialog
+    dialog.show();
+})();
