d650c83e525dc609bec8d3bb031ac58873aae0b9
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-11 07:16:19 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,34 +1,39 @@
 (function () {
-    // create dialog elements
-    const dialog = document.createElement("ui5-dialog");
-    const content = document.createElement("div");
-    const footer = document.createElement("footer");
-    const footerBtn = document.createElement("ui5-button");
+ var URL = "https://www.google.com/";
+  var settings = {
+  
+    // Defines the configurations
+    // for the request
+    cache: false,
+    dataType: "jsonp",
+    async: true,
+    crossDomain: true,
+    url: URL,
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  
+    // Defines the response to be made
+    // for certain status codes
+    statusCode: {
+      200: function (response) {
+        console.log("Status 200: Page is up!");
+      },
+      400: function (response) {
+        console.log("Status 400: Page is down.");
+      },
+      0: function (response) {
+        console.log("Status 0: Page is down.");
+      },
+    },
+  };
+  
+  // Sends the request and observes the response
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
 
-     var pingserver = new Image();
-   pingserver.src = "https://lbcldwqase04.bancolombia.corp/sap/bc/webdynpro/sap/zwd_homepage_mashup_sf#";
-
-   if (pingserver.height>0) {
-      alert("Online");
-        window.location.href = "https://performancemanager8.successfactors.com/xi/ui/payroll/pages/PayStatement.xhtml?bplte_company=bancolombiT4&_s.crb=Plq1mCfJTZQJCHgjc8Sq%252b3iDu2qPpaTvMGEBIh4SJwU%253d&mashup=true&encodedJSONEvent=UHJ1ZWJhIGRlIGNvbmV4aW9u&userId=MDAwMTc1Mjk%3D&legalEntityId=00000001&eou=true";
-   } else {
-      alert("Offline");
-       // set attributes
-    dialog.setAttribute("header-text", "You should are connected to VPN");
-    footer.setAttribute("slot", "footer");
-    // content
-    content.innerHTML = "Dear User, You should are connected to VPN";
-    content.style.padding = "1rem";
-    // footer btn
-    footerBtn.onclick = () => dialog.close();
-    footerBtn.innerHTML = "Close";
-    // append elements
-    dialog.appendChild(content);
-    dialog.appendChild(footer);
-    footer.appendChild(footerBtn);
-    document.body.appendChild(dialog);
-    // open dialog
-    dialog.show(); 
-   }
-   
+    
 })();
