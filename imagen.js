ef9c4811555fe1ef95e84d9df2f94cf2a3cca682
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-14 08:17:02 -0500

    Update imagen.js

--- imagen.js
+++ imagen.js
@@ -1,6 +1,6 @@
 function ping(){
    var pingserver = new Image();
-   pingserver.src = "http://www.cristalab.com/images/header/logo.png";
+   pingserver.src = "https://lbcldwqcie04.bancolombia.corp/sap/public/bc/ur/nw5/themes/~cache-20230413060535/UR/baseLib/sap_goldreflection/img/scrollbar/ico12_v_next.gif";
 
    if (pingserver.height>0) {
       alert("Online");
