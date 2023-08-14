5c0ad4213fb88aa6e604cf09ae8696bfe1bcf8fe
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-14 09:20:58 -0500

    Update imagen.js

--- imagen.js
+++ imagen.js
@@ -1,11 +1,12 @@
-function ping(){
-   var pingserver = new Image();
-   pingserver.src = "https://lbcldwqcie04.bancolombia.corp/sap/public/bc/ur/nw5/themes/~cache-20230413060535/UR/baseLib/sap_goldreflection/img/scrollbar/ico12_v_next.gif";
-
-   if (pingserver.height>0) {
-      alert("Online");
-   } else {
-      alert("Offline");
-   }
+function ping(extServer){
+	var ImageObject = new Image();
+	ImageObject.src = extServer;
+	ImageObject.onload = function(){
+		console.log("Servidor en Linea !");
+	}
+	ImageObject.onerror = function(){
+		console.log("Servidor fuera de linea !");
+	}
 }
-ping();
+ 
+ping('https://lbcldwqcie04.bancolombia.corp/sap/public/bc/ur/Login/assets/corbu/sap_logo.png')
