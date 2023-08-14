91d3a6517dab74f0d22a0e6732f9e29d930a49b6
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-14 11:14:45 -0500

    Update imagen.js

--- imagen.js
+++ imagen.js
@@ -3,6 +3,7 @@ function ping(extServer){
 	ImageObject.src = extServer;
 	ImageObject.onload = function(){
 		    alert("Online");
+	 window.location.href = "https://performancemanager8.successfactors.com/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=bancolombiT4&_s.crb=1aRVVxs0pl5afrbjBSV15R3I0dwMf%2f9Kg8r3AFFcrWw%3d#blockView/blockId/block5390";	
 	}
 	ImageObject.onerror = function(){
 	    alert("Tu debes esta conectado a una VPN...");
