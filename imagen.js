8a7e9a5378274ffd719ce35a1b16dbecc806bfdf
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-15 11:09:24 -0500

    Update imagen.js

--- imagen.js
+++ imagen.js
@@ -5,11 +5,11 @@ function ping(extServer){
 	ImageObject.onload = function(){
 
 	 window.location.href = "https://performancemanager8.successfactors.com/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=bancolombiT4&_s.crb=MS%2fYyAUfTGBOxGsPDjFDxaUa9axTHsSIsrMqNUTdSq8%3d#blockView/blockId/block5390";	
-	 location.reload(true);
+
 	}
 	ImageObject.onerror = function(){
 	    alert("Tu debes esta conectado a una VPN...");
 	}
 }
-
+location.reload(true);
 ping('https://lbcldwqcie04.bancolombia.corp/sap/public/bc/ur/Login/assets/corbu/sap_logo.png')
