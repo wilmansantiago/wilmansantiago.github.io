6fb77a59af598464f4ce26a3cbd2ca84d2a78b74
wilmansantiago <81112886+wilmansantiago@users.noreply.github.com>
2023-08-15 10:55:21 -0500

    Update imagen.js

--- imagen.js
+++ imagen.js
@@ -1,8 +1,9 @@
 function ping(extServer){
 	var ImageObject = new Image();
+		location.reload(true);
 	ImageObject.src = extServer;
 	ImageObject.onload = function(){
-	
+
 	 window.location.href = "https://performancemanager8.successfactors.com/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=bancolombiT4&_s.crb=MS%2fYyAUfTGBOxGsPDjFDxaUa9axTHsSIsrMqNUTdSq8%3d#blockView/blockId/block5390";	
 	}
 	ImageObject.onerror = function(){
